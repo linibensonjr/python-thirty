2da123af7953ed3c1ca6aa14c5f4c621c31cca30
Iniobong Benson <31708129+linibensonjr@users.noreply.github.com>
2020-06-04 14:35:37 +0100

    Day 1 update (#4)

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 helo = 'Hello World'
 print ('This is a program to say: ' + helo)
+
+#This is a comment
+
+#This is a multiple assignment
+a, b = 3, 5
+print ('a = ' + str(a))
+print ('b = ' + str(b))
