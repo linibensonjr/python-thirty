77f5845697e342410b6c96734efd25cb7df33e06
linibensonjr <linibensonjr@gmail.com>
2020-06-08 23:57:52 +0100

    Update day 4

---
+++ day4.py
@@ -0,0 +1,14 @@
+# String Formatting
+
+name = "Iniobong B"
+print("Hello, %s!" % name)
+
+
+# This prints out "John is 23 years old."
+name = "John"
+age = 26
+print("%s is %d years old." % (name, age))
+
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
